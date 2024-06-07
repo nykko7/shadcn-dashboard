46dfad6682b94ebc678ae524a2caefc8f7a90107
Nykko <n.perezbus@gmail.com>
2024-06-07 18:34:21 -0400

    chore: add accordion and alert component

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,7 +1,48 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const items = [
+  {
+    id: "item-1",
+    question: "Is it accessible?",
+    answer: "Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    id: "item-2",
+    question: "Is it styled?",
+    answer:
+      "Yes. It comes with default styles that matches the other components' aesthetic.",
+  },
+  {
+    id: "item-3",
+    question: "Is it animated?",
+    answer:
+      "Yes. It's animated by default, but you can disable it if you prefer.",
+  },
+  {
+    id: "item-4",
+    question: "Is it responsive?",
+    answer:
+      "Yes. It's built with responsive design in mind. It works on mobile and desktop.",
+  },
+];
+
 export default function Page() {
   return (
     <div>
-      <h1>Accordion Page</h1>
+      <h1 className="text-2xl font-bold">Accordion Page</h1>
+      <Accordion type="single" collapsible className="w-full">
+        {items.map((item) => (
+          <AccordionItem value={item.id} key={item.id}>
+            <AccordionTrigger>{item.question}</AccordionTrigger>
+            <AccordionContent>{item.answer}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </div>
   );
 }
